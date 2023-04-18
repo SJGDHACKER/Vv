8b5d3e15b9f8eadd3e3c6b035e9be1788ef81b3d
dev-virous <131063717+dev-virous@users.noreply.github.com>
2023-04-18 20:48:49 +0200

    Update الساعه.py

--- plugins/الساعه.py
+++ plugins/الساعه.py
@@ -1,8 +1,7 @@
-# الكود واخده من @FPFFG معدا الصوره الوقتيه
+# الكود واخده من @FPFFG
 from pyrogram import Client, filters
 from config import app, HNDLR, redis, SUDO_ID, timezone
 from autoname import main as name
-from autoname import photo
 import time, os
 os.environ["TZ"] = timezone
 time.tzset()
@@ -10,6 +9,8 @@ time.tzset()
 @app.on_message(filters.command("تعطيل الساعه$",prefixes=f"{HNDLR}") & filters.me )
 async def clockk(c,msg):
   redis.delete(f"{SUDO_ID}clockk")
+  get = await c.get_chat("me")
+  await c.update_profile(first_name=f'{get.last_name}' ,last_name="")
   await msg.edit("• تم تعطيل الساعه")
 
 @app.on_message(filters.command("تفعيل الساعه$",prefixes=f"{HNDLR}") & filters.me )
@@ -22,14 +23,3 @@ async def unclockk(c,msg):
   redis.set(f"{SUDO_ID}clockk",my_name)
   await msg.edit("• تم تفعيل الساعه")
   await name()
-
-@app.on_message(filters.command("تعطيل الصوره الوقتيه$",prefixes=f"{HNDLR}") & filters.me )
-async def clock(c,msg):
-  redis.delete(f"{SUDO_ID}clock")
-  await msg.edit("• تم تعطيل الصوره الوقتيه")
-  
-@app.on_message(filters.command("تفعيل الصوره الوقتيه$",prefixes=f"{HNDLR}") & filters.me )
-async def unclock(c,msg):
-  redis.set(f"{SUDO_ID}clock","virus")
-  await msg.edit("• تم تفعيل الصوره الوقتيه")
-  await photo()
