707d2373956a5f268087e86c56d03892c8e5d7bd
المعلم فيرس <131063717+dev-virous@users.noreply.github.com>
2023-05-07 01:49:17 +0300

    Update حجره.py

--- plugins/حجره.py
+++ plugins/حجره.py
@@ -4,6 +4,6 @@ import asyncio
 
 @app.on_message(filters.command("حجره$", prefixes=f"{HNDLR}") & filters.me)
 async def rock(app, msg):
-	result = await app.get_inline_bot_results(USERBOT, "حجره")
+	result = await app.get_inline_bot_results(userbot, "حجره")
 	await msg.delete()
 	await app.send_inline_bot_result(msg.chat.id, result.query_id, result.results[0].id)
